e153383e9ed8e9ca0c529f88e381faa970d146cd
daveprimrose <daveprim1993@googlemail.com>
2016-10-13 19:23:15 +0100

    added .travis.yml

--- src/main/java/com/daveprimrose/Main.java
+++ src/main/java/com/daveprimrose/Main.java
@@ -3,6 +3,7 @@ package com.daveprimrose;
 public class Main {
 
     public static void main(String[] args) {
-
+        IPerson person = new Person();
+        System.out.println(person.sayHello());
     }
 }
